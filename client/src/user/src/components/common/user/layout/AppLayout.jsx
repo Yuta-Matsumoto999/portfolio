eb0fe2afce89e78eb1439a9201d9334648986bbc
Yuta-Matsumoto999 <y.matsumoto0723@gmail.com>
2022-12-31 15:01:57 +0900

    一般ユーザー側認証後ページのレイアウト作成

--- client/src/user/src/components/common/user/layout/AppLayout.jsx
+++ client/src/user/src/components/common/user/layout/AppLayout.jsx
@@ -1,6 +1,16 @@
 import React from 'react'
+import { Outlet, useLocation, useNavigate } from 'react-router-dom';
+import useGeneralAuth from '../../../../customHooks/useGeneralAuth';
 
 const AppLayout = () => {
+    const navigate = useNavigate();
+    const location = useLocation();
+    const authenticate = useGeneralAuth();
+
+    if(authenticate === false) {
+        navigate("/authenticate/login");
+    }
+
   return (
     <div>AppLayout</div>
   )
