ade7a609095b890582ace230bb93791bb8f4c267
Yuta-Matsumoto999 <y.matsumoto0723@gmail.com>
2022-12-31 15:27:24 +0900

    一般ユーザー側パスワードリセット機能実装

---
+++ server/src/app/Http/Controllers/General/Auth/ResetPasswordController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers\General\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Foundation\Auth\ResetsPasswords;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Support\Facades\Hash;
+use App\Http\Requests\User\auth\ResetPasswordRequest;
+use Illuminate\Auth\Events\PasswordReset;
+
+class ResetPasswordController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller is responsible for handling password reset requests
+    | and uses a simple trait to include this behavior. You're free to
+    | explore this trait and override any methods you wish to tweak.
+    |
+    */
+
+    public function resetPassword(ResetPasswordRequest $request)
+    {
+        $status = Password::broker('users')->reset(
+            $request->only('email', 'password', 'password_confirmation', 'token'),
+            function ($user) use ($request) {
+                $user->forceFill([
+                    'password' => Hash::make($request->password),
+                ])->save();
+    
+                event(new PasswordReset($user));
+            }
+        );
+
+        return $status == Password::PASSWORD_RESET
+                ? response()->json(['message' => 'パスワードの再設定が完了しました。', 'status' => true], 201)
+                : response()->json(['message' => 'パスワードの再設定に失敗しました。', 'status' => false], 401);
+    }
+}
