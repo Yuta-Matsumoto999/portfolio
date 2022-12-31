5f1315d41839b3d6450e0d4368fd4dd1dc0bead1
Yuta-Matsumoto999 <y.matsumoto0723@gmail.com>
2022-12-31 13:23:37 +0900

    管理側パスワードリセット要求機能作成

---
+++ server/src/app/Http/Controllers/Admin/Auth/ForgotPasswordController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
+use App\Http\Requests\Admin\auth\ForgotPasswordRequest;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Validation\ValidationException;
+
+class ForgotPasswordController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller is responsible for handling password reset emails and
+    | includes a trait which assists in sending these notifications from
+    | your application to your users. Feel free to explore this trait.
+    |
+    */
+
+    public function sendResetLinkEmail(ForgotPasswordRequest $request): JsonResponse
+    {
+        $status = Password::broker('admins')->sendResetLink($request->only('email'));
+
+        if ($status !== Password::RESET_LINK_SENT) {
+            throw ValidationException::withMessages([
+                'email' => trans($status),
+            ]);
+        }
+
+        return new JsonResponse([
+            'message' => trans($status),
+        ]);
+    }
+}
