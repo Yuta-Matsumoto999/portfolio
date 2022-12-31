1656f88fb566a8ccacdfaa35722170c55a2117d5
Yuta-Matsumoto999 <y.matsumoto0723@gmail.com>
2022-12-31 15:26:22 +0900

    管理側のパスワードリセット通知のディレクトリ変更

--- server/src/app/Models/Admin.php
+++ server/src/app/Models/Admin.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
-use App\Notifications\Api\Auth\ResetPasswordNotification;
+use App\Notifications\Api\Admin\Auth\ResetPasswordNotification;
 
 class Admin extends Authenticatable
 {
