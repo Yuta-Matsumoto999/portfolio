a4fb14f4e1fe6e54abb4694d5d321f5e8a10cda2
Yuta-Matsumoto999 <y.matsumoto0723@gmail.com>
2023-01-09 05:17:34 +0900

    userテーブルに生年月日columを追加

--- server/src/database/seeders/UserSeeder.php
+++ server/src/database/seeders/UserSeeder.php
@@ -21,31 +21,36 @@ class UserSeeder extends Seeder
                 "organization_id" => 1,
                 "name" => "testUser",
                 "email" => "test@hoge.com",
-                "password" => Hash::make('password')
+                "password" => Hash::make('password'),
+                "birthday" => "2005/07/04"
             ],
             [
                 "organization_id" => 1,
                 "name" => "testUser2",
                 "email" => "test2@hoge.com",
-                "password" => Hash::make('password')
+                "password" => Hash::make('password'),
+                "birthday" => "2007/07/04"
             ],
             [
                 "organization_id" => 1,
                 "name" => "testUser3",
                 "email" => "test3@hoge.com",
-                "password" => Hash::make('password')
+                "password" => Hash::make('password'),
+                "birthday" => "2006/07/04"
             ],
             [
                 "organization_id" => 1,
                 "name" => "testUser4",
                 "email" => "test4@hoge.com",
-                "password" => Hash::make('password')
+                "password" => Hash::make('password'),
+                "birthday" => "2006/03/31"
             ],
             [
                 "organization_id" => 1,
                 "name" => "testUser5",
                 "email" => "test5@hoge.com",
-                "password" => Hash::make('password')
+                "password" => Hash::make('password'),
+                "birthday" => "2005/04/02"
             ]
         ]);
     }
