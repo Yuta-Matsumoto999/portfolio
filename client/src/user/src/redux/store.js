9254bf4919f22af75e981bff4804d70fe6e2f5d5
Yuta-Matsumoto999 <y.matsumoto0723@gmail.com>
2023-01-09 05:20:57 +0900

    team管理用reducer作成

--- client/src/user/src/redux/store.js
+++ client/src/user/src/redux/store.js
@@ -3,11 +3,15 @@ import { useReducer } from "react";
 import adminReducer from "./features/adminSlice";
 import generalReducer from "./features/generalSlice";
 import sidebarReducer from "./features/sidebarSlice";
+import teamReducer from "./features/teamSlice";
+import teamUserReducer from "./features/teamUserSlice";
 
 export const store = configureStore({
     reducer: {
         admin: adminReducer,
         general: generalReducer,
-        sidebar: sidebarReducer
+        sidebar: sidebarReducer,
+        team: teamReducer,
+        teamUser: teamUserReducer,
     }
 });
